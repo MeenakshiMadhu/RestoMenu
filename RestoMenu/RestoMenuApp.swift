1b83273a9c56fbd49a96bbf77e5cbc716ed9d2c1
Meenakshi Madhu <meenakshimadhu00@gmail.com>
2025-06-04 14:19:51 -0400

    Initial Commit

---
+++ RestoMenu/RestoMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestoMenuApp.swift
+//  RestoMenu
+//
+//  Created by Meenakshi Madhu on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct RestoMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RestoMenuTests/RestoMenuTests.swift
@@ -0,0 +1,16 @@
+//
+//  RestoMenuTests.swift
+//  RestoMenuTests
+//
+//  Created by Meenakshi Madhu on 6/4/25.
+//
+
+import Testing
+
+struct RestoMenuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
